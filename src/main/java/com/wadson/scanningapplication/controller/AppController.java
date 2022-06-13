1b363b13da783d98b576437543b1f52963a822a8
dutervil <dwadson3@gmail.com>
2022-06-12 19:48:27 -0700

    ok

--- src/main/java/com/wadson/scanningapplication/controller/AppController.java
+++ src/main/java/com/wadson/scanningapplication/controller/AppController.java
@@ -1,13 +1,15 @@
 package com.wadson.scanningapplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("nom","WADSON");
         return "index";
     }
 
